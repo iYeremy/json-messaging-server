c2ce37df0d660b75af68b3ed9945ad09dca232c8
Yeremy <yeremyvegap@gmail.com>
2025-11-21 19:25:29 -0500

    chore: agregar funciones vacias para futuro desarrollo

--- cliente.py
+++ cliente.py
@@ -11,3 +11,18 @@ import json
 # PARAMETROS DE RED
 HOST = "127.0.0.1" 
 PORT = 50000
+
+def enviar(sock, paquete):
+    pass
+
+def registrar_mensaje(sock):
+    pass
+
+def listar_mensajes(sock):
+    pass
+
+def cerrar_conexion(sock):
+    pass
+
+def iniciar_cliente():
+    pass
